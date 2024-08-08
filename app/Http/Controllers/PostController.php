1dd70a410c3acf4e00ab1589263b0b5f68b0c168
Luis Parrado <luisprmat@gmail.com>
2024-08-08 02:24:11 -0500

    Post index, show and store logic with Eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,15 +13,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return Post::latest()->get();
     }
 
     /**
@@ -29,7 +21,7 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
-        //
+        return Post::create($request->validated() + ['user_id' => 1]);
     }
 
     /**
@@ -37,15 +29,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
-        //
+        return $post;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('posts', [PostController::class, 'store'])->name('posts.store');
